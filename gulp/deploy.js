d45a6bea2c63f927e82b8cf1fdba81b096bb94b1
Joey Nichols <joeyquarters@gmail.com>
2017-09-30 21:23:57 -0400

    Make exec play nicely with gulp

--- gulp/deploy.js
+++ gulp/deploy.js
@@ -8,32 +8,24 @@ module.exports = (gulp) => {
   /**
    * Sync the site (_site/) with the S3 bucket
    */
-  gulp.task('deploy:site', () => {
+  gulp.task('deploy:site', (cb) => {
     const command = 'aws s3 sync _site/ s3://www.westeleventh.media/';
-    exec(command, (error, stdout, stderr) => {
-      if (error) {
-        gutil.log(gutil.colors.magenta(`exec error: ${error}`));
-        return;
-      }
-
+    return exec(command, (error, stdout, stderr) => {
       gutil.log(stdout);
       gutil.log(gutil.colors.magenta(stderr));
+      cb(error);
     });
   });
 
   /**
    * Sync the _audio/final folder with the S3 bucket
    */
-  gulp.task('deploy:audio', () => {
+  gulp.task('deploy:audio', (cb) => {
     const command = 'aws s3 sync _audio/final/ s3://episodes.westeleventh.media/ --acl \'public-read\'';
-    exec(command, (error, stdout, stderr) => {
-      if (error) {
-        gutil.log(gutil.colors.magenta(`exec error: ${error}`));
-        return;
-      }
-
+    return exec(command, (error, stdout, stderr) => {
       gutil.log(stdout);
       gutil.log(gutil.colors.magenta(stderr));
+      cb(error);
     });
   });
 
