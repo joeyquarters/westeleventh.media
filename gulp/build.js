b2f079378d532ada41d4445a5323a687b8ed6436
Joey Nichols <joeyquarters@gmail.com>
2017-11-17 18:47:53 -0500

    Make build task asynchronous

--- gulp/build.js
+++ gulp/build.js
@@ -9,16 +9,17 @@ module.exports = (gulp) => {
    * Task: build
    * Build our site for production
    */
-  gulp.task('build', () => {
+  gulp.task('build', (cb) => {
     const buildCommand = 'JEKYLL_ENV=production bundle exec jekyll build';
     exec(buildCommand, (error, stdout, stderr) => {
       if (error) {
         gutil.log(gutil.colors.magenta(`exec error: ${error}`));
-        return;
+        return cb(error);
       }
 
       gutil.log(stdout);
       gutil.log(gutil.colors.magenta(stderr));
+      cb()
     });
   });
 
